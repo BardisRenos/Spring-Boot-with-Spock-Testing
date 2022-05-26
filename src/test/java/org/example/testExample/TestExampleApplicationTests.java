5e68a9662f0585745facb9e5d2ba8185fe4d95f8
Renos Bardis <renosbardis@gmail.com>
2022-05-26 09:42:15 +0200

    "Adding new files"

---
+++ src/test/java/org/example/testExample/TestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
