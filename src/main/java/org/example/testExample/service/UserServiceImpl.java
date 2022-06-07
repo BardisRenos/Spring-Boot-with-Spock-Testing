fda94ad5c69682cc2010a33ef80da12f49872a2d
Renos Bardis <renosbardis@gmail.com>
2022-06-07 23:07:49 +0200

    feature: Adding new handling exception

--- src/main/java/org/example/testExample/service/UserServiceImpl.java
+++ src/main/java/org/example/testExample/service/UserServiceImpl.java
@@ -2,10 +2,12 @@ package org.example.testExample.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.testExample.dao.UserRepository;
+import org.example.testExample.exception.UserNotFoundException;
 import org.example.testExample.resources.User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -19,17 +21,26 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User getUserById(Long id) {
+    public User getUserById(Long id) throws UserNotFoundException {
+        if (Objects.isNull(userRepository.findByUserId(id))) {
+            throw new UserNotFoundException(String.format("The user not found with the id: %s ", id));
+        }
         return userRepository.findByUserId(id);
     }
 
     @Override
-    public User getUserByLastName(String lastName) {
+    public User getUserByLastName(String lastName) throws UserNotFoundException {
+        if (Objects.isNull(userRepository.findByLastName(lastName))) {
+            throw new UserNotFoundException(String.format("The user not found with the last name: %s ", lastName));
+        }
         return userRepository.findByLastName(lastName);
     }
 
     @Override
-    public List<User> getUserByFirstName(String firstName) {
+    public List<User> getUserByFirstName(String firstName) throws UserNotFoundException {
+        if (Objects.isNull(userRepository.findByFirstName(firstName))) {
+            throw new UserNotFoundException(String.format("The user not found with the first name: %s ", firstName));
+        }
         return userRepository.findByFirstName(firstName);
     }
 }
